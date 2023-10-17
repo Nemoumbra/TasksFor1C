4a6a8c27d81b5202d377d7133e6bed9cdc1546dc
Nemoumbra <garbuzov.nickolay@gmail.com>
2023-10-17 22:31:55 +0300

    NeoClient chat tests

---
+++ NeoClient/NeoClient/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace NeoClient
+{
+    class Client {
+        TcpClient client = null;
+        byte[] data = null;
+        NetworkStream stream = null;
+        
+    }
+}
--- NeoClient/NeoClient/MainWindow.xaml.cs
+++ NeoClient/NeoClient/MainWindow.xaml.cs
@@ -21,5 +21,47 @@ namespace NeoClient {
         public MainWindow() {
             InitializeComponent();
         }
+
+        void hide(UIElement elem) {
+            elem.Visibility = Visibility.Hidden;
+        }
+        void show(UIElement elem) {
+            elem.Visibility = Visibility.Visible;
+        }
+
+
+        private void input_textbox_MouseEnter(object sender, MouseEventArgs e) {
+
+        }
+
+        private void input_textbox_MouseLeave(object sender, MouseEventArgs e) {
+
+        }
+
+        private void send_button_Click(object sender, RoutedEventArgs e) {
+            
+        }
+
+        private void localhost_Checked(object sender, RoutedEventArgs e) {
+            ip_label.IsEnabled = false;
+            ip_textbox.IsEnabled = false;
+        }
+
+        private void localhost_Unchecked(object sender, RoutedEventArgs e) {
+            ip_label.IsEnabled = true;
+            ip_textbox.IsEnabled = true;
+        }
+
+        private void connect_button_Click(object sender, RoutedEventArgs e) {
+            hide(connect_button);
+            hide(ip_textbox);
+            hide(ip_label);
+            hide(localhost_checkbox);
+            hide(port_textbox);
+            hide(port_label);
+            show(input_textbox);
+            show(send_button);
+            show(listbox_msg);
+        }
     }
 }
